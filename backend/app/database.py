be1db97f9ad5b9788bcb3a77eec023b9073faf85
ZypherNight <shudhanshbhardwaj10@gmail.com>
2026-05-21 14:16:39 +0530

    chore: add requirements and fix db string for prod

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,8 +5,12 @@ import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./bookleaf.db")
 
+connect_args = {}
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    connect_args["check_same_thread"] = False
+
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
